65e1c071f7c9797f4c167a59776f8105d84f68f1
Michael <muniu.dev@gmail.com>
2023-09-26 01:21:48 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+
+    console.log(`This Saturday, I want to ${activity}!`);
+}
+saturdayFun("bathe my dog")
+
+const mondayWork = function (fn = "go to the office"){
+    console.log(`This Monday, I will ${fn}`);
+}
+
+function wrapAdjective(){
+    return function(prm = "special"){
+        return `You are ${prm}`
+    }
+        
+}
